4cbb23962b8ea449c890fb990e000bc19be29e40
Jason Kriss <jasonkriss@gmail.com>
2015-03-23 10:16:51 -0700

    turn off format, and turn on shallow nesting for all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Clubhouse::Engine.routes.draw do
-  resources :organizations, except: [:new, :edit], shallow: true do
-    match ":id", action: :check, on: :collection, via: :head
+  scope shallow: true, format: false do
+    resources :organizations, except: [:new, :edit] do
+      match ":id", action: :check, on: :collection, via: :head
 
-    resources :invitations, except: [:new, :edit]
+      resources :invitations, except: [:new, :edit]
 
-    resources :memberships, except: [:new, :edit]
+      resources :memberships, except: [:new, :edit]
+    end
   end
 end
