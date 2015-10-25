fd8962f9af21ebe19d27e7b7a9b1c5e1520017fb
Jason Kriss <jasonkriss@gmail.com>
2015-10-24 17:18:02 -0700

    allow . in Organization#name checks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Clubhouse::Engine.routes.draw do
   scope shallow: true, format: false do
-    resources :organizations, except: [:new, :edit] do
+    resources :organizations, except: [:new, :edit], id: /[^\/\?]+/ do
       get :check, on: :member
 
       resources :invitations, except: [:new, :edit]
